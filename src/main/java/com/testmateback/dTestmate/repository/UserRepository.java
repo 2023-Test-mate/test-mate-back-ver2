d2e89c2faa3586f6ca49ccccdc2e1f5d1cd3bace
mic050r <103114387+mic050r@users.noreply.github.com>
2023-09-11 19:19:52 +0900

    feat : 로그인 기능 추가

--- src/main/java/com/testmateback/dTestmate/repository/UserRepository.java
+++ src/main/java/com/testmateback/dTestmate/repository/UserRepository.java
@@ -9,7 +9,6 @@ import java.util.List;
 @Repository
 public interface UserRepository extends JpaRepository<Users, Long> {
 
-    Users findByEmail(String email);
-    // 모든 사용자 조회
-    List<Users> findAll();
+    Users findByEmail(String email); // 이메일로 사용자 조회
+
 }
