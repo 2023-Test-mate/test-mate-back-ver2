d83312ef5beca1ed384cf3cd45aa3163b59d6114
mic050r <103114387+mic050r@users.noreply.github.com>
2023-09-13 23:02:56 +0900

    feat : 오답노트 테이블 수정

--- src/main/java/com/testmateback/dTestmate/repository/HomeRepository.java
+++ src/main/java/com/testmateback/dTestmate/repository/HomeRepository.java
@@ -4,6 +4,7 @@ import com.testmateback.dTestmate.entity.Home;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface HomeRepository extends JpaRepository<Home, Long> {
 
