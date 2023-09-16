0ec4e6344b238931a412180571dbab52033c560a
mic050r <103114387+mic050r@users.noreply.github.com>
2023-09-16 19:43:20 +0900

    chore : semester grade로 변경

--- src/main/java/com/testmateback/dTestmate/controller/DMakerController.java
+++ src/main/java/com/testmateback/dTestmate/controller/DMakerController.java
@@ -1,11 +1,9 @@
 package com.testmateback.dTestmate.controller;
 
+import com.testmateback.dTestmate.dao.*;
 import com.testmateback.dTestmate.dto.*;
 import com.testmateback.dTestmate.entity.*;
-import com.testmateback.dTestmate.repository.EditSubjectRepository;
-import com.testmateback.dTestmate.repository.GoalRepository;
-import com.testmateback.dTestmate.repository.TestInfoRepository;
-import com.testmateback.dTestmate.repository.UserRepository;
+import com.testmateback.dTestmate.repository.*;
 import com.testmateback.dTestmate.service.*;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -126,25 +125,153 @@ public class DMakerController {
 
         @Autowired
         private TestInfoRepository testInfoRepository;
+        @Autowired
+        private EditSubjectRepository editSubjectRepository; // EditSubject 테이블에 접근하기 위한 리포지토리 추가
+
 
         @GetMapping("/details")
-        public GoalDetails getGoalDetails(@RequestParam String indexes) {
-            GoalDetails goalDetails = new GoalDetails();
-            goalDetails.setIndexes(indexes);
+        public List<GoalDetails> getGoalDetails(@RequestParam String indexes) {
+            List<GoalDetails> goalDetailsList = new ArrayList<>();
 
             // testInfoRepository 인스턴스를 사용하여 findByIndexes 메서드 호출
-            TestInfo testInfo = (TestInfo) testInfoRepository.findByIndexes(indexes).orElse(null);
+            List<TestInfo> testInfoList = testInfoRepository.findByIndexes(indexes);
 
-            if (testInfo != null) {
-                goalDetails.setGoalSubject(testInfo.getSubject());
-                goalDetails.setGoalSemester(testInfo.getSemester());
+            EditSubject editSubject = editSubjectRepository.findBySubject(indexes);
+
+            for (TestInfo testInfo : testInfoList) {
+                GoalDetails goalDetails = new GoalDetails();
+                goalDetails.setIndexes(indexes);
+                goalDetails.setGoalSubject(editSubject.getSubject());
+                goalDetails.setGoalGrade(testInfo.getGrade());
+
+                // EditSubject 테이블에서 subject에 해당하는 photo를 가져옵니다.
+                if (editSubject != null) {
+                    goalDetails.setSubjectImg(editSubject.getPhoto());
+                }
+
+                // goalRepository를 사용하여 해당 subject의 goal 개수를 구합니다.
+                List<Goal> goalsBySubject = goalRepository.findBySubject(editSubject.getSubject());
+                List<Goal> checkedGoalsBySubject = goalRepository.findBySubjectAndChecksTrue(editSubject.getSubject());
+
+                goalDetails.setTotalGoals(goalsBySubject.size());
+                goalDetails.setCheckedGoals(checkedGoalsBySubject.size());
+                goalDetailsList.add(goalDetails);
             }
 
-            goalDetails.setTotalGoals(goalRepository.count());
-            goalDetails.setCheckedGoals(goalRepository.countByChecksIsTrue());
-
-            return goalDetails;
+            return goalDetailsList;
         }
 
+
+        @GetMapping("/check-lists")
+        public GoalCheckResponse getGoalCheck(
+                @RequestParam String indexes,
+                @RequestParam String subject,
+                @RequestParam String grade
+        ) {
+            GoalCheckResponse response = new GoalCheckResponse();
+
+            // Checked Goals 조회 (checks가 1인 것)
+            List<Goal> checkedGoals = goalRepository.findByIndexesAndSubjectAndGradeAndChecks(indexes, subject, grade, true);
+            List<String> checkedGoalStrings = new ArrayList<>();
+            for (Goal goal : checkedGoals) {
+                checkedGoalStrings.add(goal.getGoal());
+            }
+            response.setCheckedGoals(checkedGoalStrings);
+
+            // No Goals 조회 (checks가 0인 것)
+            List<Goal> noGoals = goalRepository.findByIndexesAndSubjectAndGradeAndChecks(indexes, subject, grade, false);
+            List<String> noGoalStrings = new ArrayList<>();
+            for (Goal goal : noGoals) {
+                noGoalStrings.add(goal.getGoal());
+            }
+            response.setNoGoals(noGoalStrings);
+
+            return response;
+        }
     }
+
+    @RestController
+    @RequestMapping("/home")
+    public class HomeController {
+
+        @Autowired
+        private EditSubjectRepository editSubjectRepository;
+
+        @Autowired
+        private TestInfoRepository testInfoRepository;
+
+        @Autowired
+        private WrongNoteRepository wrongNoteRepository;
+
+        @Autowired
+        private HomeRepository homeRepository;
+
+        @GetMapping("/get-home-info")
+        public HomeInfoResponse getHomeInfo(
+                @RequestParam String indexes,
+                @RequestParam String subject,
+                @RequestParam String grade
+        ) {
+            HomeInfoResponse response = new HomeInfoResponse();
+
+            // EditSubject에서 indexes와 subject에 해당하는 데이터를 가져와서 리스트로 만듭니다.
+            List<EditSubject> editSubjects = editSubjectRepository.findByIndexesAndSubjectAndGrade(indexes, subject, grade);
+
+            List<EditSubjectData> editSubjectDataList = new ArrayList<>();
+            for (EditSubject editSubject : editSubjects) {
+                EditSubjectData data = new EditSubjectData();
+                data.setSubject(editSubject.getSubject());
+                data.setPhoto(editSubject.getPhoto());
+                editSubjectDataList.add(data);
+            }
+            response.setEditSubjectList(editSubjectDataList);
+
+            // TestInfo에서 indexes와 subject에 해당하는 데이터를 가져와서 리스트로 만듭니다.
+            List<TestInfo> testInfos = testInfoRepository.findByIndexesAndSubject(indexes, subject);
+
+            List<TestInfoData> testInfoDataList = new ArrayList<>();
+            for (TestInfo testInfo : testInfos) {
+                TestInfoData data = new TestInfoData();
+                data.setSubject(testInfo.getSubject());
+                data.setgrade(testInfo.getGrade());
+                data.setScore(testInfo.getScore());
+                data.setDates(testInfo.getDates());
+                data.setLevels(testInfo.getLevels());
+                data.setTarget(testInfo.getTarget());
+                testInfoDataList.add(data);
+            }
+            response.setTestInfoList(testInfoDataList);
+
+            // WrongNote에서 indexes, grade, subject에 해당하는 데이터를 가져와서 처리합니다.
+            List<WrongNote> wrongNotes = wrongNoteRepository.findByIndexesAndGradeAndSubject(indexes, grade, subject);
+
+            List<WrongNoteData> wrongNoteDataList = new ArrayList<>();
+            for (WrongNote wrongNote : wrongNotes) {
+                WrongNoteData data = new WrongNoteData();
+                // WrongNote 엔터티에 있는 필드들을 가져와서 data 객체에 설정해주세요.
+                data.setReasoncheck(wrongNote.getReasoncheck());
+
+                data.setReason(wrongNote.getReason());
+
+                wrongNoteDataList.add(data);
+            }
+            response.setWrongNoteList(wrongNoteDataList);
+
+            // Home에서 indexes, grade, subject가 fail인 데이터를 가져와서 처리합니다.
+            List<Home> homeData = homeRepository.findByIndexesAndGradeAndSubject(indexes, grade, subject);
+
+            List<HomeData> homeDataList = new ArrayList<>();
+            for (Home home : homeData) {
+                HomeData data = new HomeData();
+                // Home 엔터티에 있는 필드들을 가져와서 data 객체에 설정해주세요.
+                data.setFail(home.getFail());
+
+                homeDataList.add(data);
+            }
+            response.setHomeList(homeDataList);
+
+            return response;
+        }
+    }
+
 }
