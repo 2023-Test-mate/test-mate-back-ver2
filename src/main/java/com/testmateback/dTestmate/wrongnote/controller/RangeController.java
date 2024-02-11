056b242f460216dab18af9f2e124037b0264bec1
mic050r <103114387+mic050r@users.noreply.github.com>
2024-02-12 04:57:08 +0900

    ✨ [feat] : 범위 crud api 완성

--- src/main/java/com/testmateback/dTestmate/wrongnote/controller/RangeController.java
+++ src/main/java/com/testmateback/dTestmate/wrongnote/controller/RangeController.java
@@ -1,4 +1,58 @@
 package com.testmateback.dTestmate.wrongnote.controller;
 
+import com.testmateback.dTestmate.wrongnote.dto.CreateRangeReq;
+import com.testmateback.dTestmate.wrongnote.entity.Range;
+import com.testmateback.dTestmate.wrongnote.service.RangeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/ranges")
 public class RangeController {
+
+    private final RangeService rangeService;
+
+    @Autowired
+    public RangeController(RangeService rangeService) {
+        this.rangeService = rangeService;
+    }
+
+    /*
+        @ 오답노트 - 범위 추가
+        post api/ranges
+     */
+    @PostMapping
+    public Range createRange(@RequestBody CreateRangeReq createRangeReq) {
+        return rangeService.createRange(createRangeReq);
+    }
+
+    /*
+        @ 오답노트 - 모든 범위 가져오기
+        get api/ranges
+     */
+    @GetMapping
+    public List<Range> getRangesByUserId() {
+        return rangeService.getRangesByUserId();
+    }
+
+    /*
+        @ 오답노트 - 범위 수정하기
+        put api/ranges:rangeId
+     */
+    @PutMapping("/{rangeId}")
+    public Range updateRange(@PathVariable int rangeId, @RequestBody Range range) {
+        return rangeService.updateRange(rangeId, range);
+    }
+
+    /*
+       @ 오답노트 - 범위 삭제하기
+       delete api/ranges:rangeId
+    */
+    @DeleteMapping("/{rangeId}")
+    public void deleteRange(@PathVariable int rangeId) {
+        rangeService.deleteRange(rangeId);
+    }
 }
--- src/main/java/com/testmateback/dTestmate/wrongnote/service/RangeService.java
+++ src/main/java/com/testmateback/dTestmate/wrongnote/service/RangeService.java
@@ -1,4 +1,67 @@
 package com.testmateback.dTestmate.wrongnote.service;
 
+import com.testmateback.dTestmate.wrongnote.dto.CreateRangeReq;
+import com.testmateback.dTestmate.wrongnote.entity.Range;
+import com.testmateback.dTestmate.wrongnote.repository.RangeRepository;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class RangeService {
+
+    private final RangeRepository rangeRepository;
+    private final HttpSession session;
+    private final static String LOGIN_SESSION_KEY = "USER_ID";
+
+    @Autowired
+    public RangeService(RangeRepository rangeRepository, HttpSession session) {
+        this.rangeRepository = rangeRepository;
+        this.session = session;
+    }
+
+    // 범위 추가
+    public Range createRange(CreateRangeReq createRangeReq) {
+        Long userId = getCurrentUserIdFromSession();
+        Range range = new Range();
+        range.setUserId(userId);
+        range.setRange(createRangeReq.getRange());
+        return rangeRepository.save(range);
+    }
+
+    // 범위 가져오기
+    public List<Range> getRangesByUserId() {
+        Long userId = getCurrentUserIdFromSession();
+        return rangeRepository.findByUserId(userId);
+    }
+
+
+    // 범위 수정
+    public Range updateRange(int rangeId, Range updatedRange) {
+        Range existingRange = rangeRepository.findById(rangeId).orElse(null);
+        if (existingRange != null) {
+            existingRange.setRange(updatedRange.getRange());
+            return rangeRepository.save(existingRange);
+        } else {
+            return null; // or throw an exception, depending on your requirements
+        }
+    }
+
+    // 범위 삭제
+    public void deleteRange(int rangeId) {
+        rangeRepository.deleteById(rangeId);
+    }
+
+    private Long getCurrentUserIdFromSession() {
+        // 세션에서 사용자 ID를 가져오는 로직
+        Object userIdAttribute = session.getAttribute(LOGIN_SESSION_KEY);
+
+        if (userIdAttribute != null) {
+            return (Long) userIdAttribute;
+        } else {
+            throw new RuntimeException("User not logged in");
+        }
+    }
 }
