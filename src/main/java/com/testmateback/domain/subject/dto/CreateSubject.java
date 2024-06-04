6725a29d51cf04c3e5645dd6aba0345ee59f6a5a
mic050r <s2216@e-mirim.hs.kr>
2024-06-05 07:57:18 +0900

    ✨[feat] : 과목 생성 이미지 string으로 수정하기

--- src/main/java/com/testmateback/domain/subject/dto/CreateSubject.java
+++ src/main/java/com/testmateback/domain/subject/dto/CreateSubject.java
@@ -3,7 +3,6 @@ package com.testmateback.domain.subject.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.web.multipart.MultipartFile;
 
 @Data
 @AllArgsConstructor
@@ -12,6 +11,6 @@ public class CreateSubject {
     private int userId;
     private int grade;
     private String subjectName;
-    private MultipartFile img;
+    private String img;
 
 }
